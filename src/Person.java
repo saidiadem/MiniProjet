922c95963b287f034a99028680cdf894e9ebc366
saidi adem <saidiadem@insat.u-carthage.tn>
2024-01-01 18:52:09 +0100

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,40 @@
+public class User {
+    private String username;
+    private String password;
+    private UserType userType;
+
+    public User(String username, String password, UserType userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+    public boolean isAdmin() {
+        return userType == UserType.ADMIN;
+    }
+}
